9cf10d9973d102f7ab5575cbc0bd3d55bc51fa09
Jim Musyoki <jim.musyoki@strathmore.edu>
2021-04-09 20:42:53 +0300

    Final commit of project

--- js/script.js
+++ js/script.js
@@ -2,159 +2,106 @@ const calculate = function calculate(ev)
 {
   ev.preventDefault();
 
-  var period =(document.getElementById('period').value);
+  var akanName = function(period,year,month,date)
+  {
+    return (Math.round(
+        (((period/4)-(2*period-1)) +
+        (5*year/4) +
+        (26*(month+1)/10) +
+        (date)) %7 ));
+  };
+
+  var period =parseInt(document.getElementById('period').value);
   console.log(period);
-  var year =(document.getElementById('year').value);
+  var year =parseInt(document.getElementById('year').value);
   console.log(year);
-  var month =(document.getElementById('month').value);
+  var month =parseInt(document.getElementById('month').value);
   console.log(month);
-  var date =(document.getElementById('date').value);
+  var date =parseInt(document.getElementById('date').value);
   console.log(date);
-  var gender =(document.getElementById('gender').value);
+  var gender =parseInt(document.getElementById('gender').value);
   console.log(gender);
 
-  function akanName(period, year, month, date)
-    {
-      day = ((  (period/4)-(2*period-1) +
-                (5*year/4) +
-                (26*(month+1)/10) +
-                (date)) %7);
-      return day;
-    }
+  var day;
+  day = akanName(period,year,month,date);
+  var weekdays = ['Sunday', 'Monday', 'Tuesday',
+                  'Wednesday', 'Thursday', 'Friday',
+                  'Saturday'];
+  var male = ['Kwasi', 'Kwadwo', 'Kwabena',
+              'Kwaku', 'Yaw', 'Kofi','Kwame'];
 
-    if(gender === 1)
-        {
-          if (day === 0) {
-            console.log("Your Akan name is Kwasi");
-            alert("Your Akan name is Kwasi");
-            // result.innerHTML = 'Your Akan name is Kwasi';
+  var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua',
+                'Yaa', 'Afua', 'Ama'];
 
-          }else if (day === 1) {
-            console.log("Your Akan name is Kwadwo");
-            alert("Your Akan name is Kwadwo");
-            // result.innerHTML = 'Your Akan name is Kwadwo';
+  if(gender === 1 && day === 0)
+  {
+    alert('Day of birth is '+weekdays[0]);
+    alert("Your Akan name is "+male[0]);
+  }else if (gender === 2 && day === 0) {
+    alert('Day of birth is '+weekdays[0]);
+    alert("Your Akan name is "+female[0]);
+  }
 
-          }else if (day === 2) {
-            console.log("Your Akan name is Kwabena");
-            alert("Your Akan name is Kwabena");
-            // result.innerHTML = 'Your Akan name is Kwabena';
+  if(gender === 1 && day === 1)
+  {
+    alert('Day of birth is '+weekdays[1]);
+    alert("Your Akan name is "+male[1]);
+  }else if (gender === 2 && day === 1) {
+    alert('Day of birth is '+weekdays[1]);
+    alert("Your Akan name is "+female[1]);
+  }
 
-          }else if (day === 3) {
-            console.log("Your Akan name is Kwaku");
-            alert("Your Akan name is Kwaku");
-            // result.innerHTML = 'Your Akan name is Kwaku';
+  if(gender === 1 && day === 2)
+  {
+    alert('Day of birth is '+weekdays[2]);
+    alert("Your Akan name is "+male[2]);
+  }else if (gender === 2 && day === 2) {
+    alert('Day of birth is '+weekdays[2]);
+    alert("Your Akan name is "+female[2]);
+  }
 
-          }else if (day === 4) {
-            console.log("Your Akan name is Yaw");
-            alert("Your Akan name is Yaw");
-            // result.innerHTML = 'Your Akan name is Yaw';
+  if(gender === 1 && day === 3)
+  {
+    alert('Day of birth is '+weekdays[3]);
+    alert("Your Akan name is "+male[3]);
+  }else if (gender === 2 && day === 3) {
+    alert('Day of birth is '+weekdays[3]);
+    alert("Your Akan name is "+female[3]);
+  }
 
-          }else if (day === 5) {
-            console.log("Your Akan name is Kofi");
-            alert("Your Akan name is Kofi");
-            // result.innerHTML = 'Your Akan name is Kofi';
+  if(gender === 1 && day === 4)
+  {
+    alert('Day of birth is '+weekdays[4]);
+    alert("Your Akan name is "+male[4]);
+  }else if (gender === 2 && day === 4) {
+    alert('Day of birth is '+weekdays[4]);
+    alert("Your Akan name is "+female[4]);
+  }
 
-          }else {
-            console.log("Your Akan name is Kwame");
-            alert("Your Akan name is Kwame");
-            // result.innerHTML = 'Your Akan name is Kwame';
+  if(gender === 1 && day === 5)
+  {
+    alert('Day of birth is '+weekdays[5]);
+    alert("Your Akan name is "+male[5]);
+  }else if (gender === 2 && day === 5) {
+    alert('Day of birth is '+weekdays[5]);
+    alert("Your Akan name is "+female[5]);
+  }
 
-          }
-        }
+  if(gender === 1 && day === 6)
+  {
+    alert('Day of birth is '+weekdays[6]);
+    alert("Your Akan name is "+male[6]);
+  }else if (gender === 2 && day === 6) {
+    alert('Day of birth is '+weekdays[6]);
+    alert("Your Akan name is "+female[6]);
+  }
+}
+const clear = function clear(e)
+{
+  e.reset();
 }
 
 document.addEventListener('DOMContentLoaded', function load()
 {
-  document.getElementById('button').addEventListener('click', calculate);
+  document.getElementById('button').addEventListener('click', calculate, clear);
 });
-
-
-
-
-// var form = document.getElementById('nameForm');
-// form.onsubmit = function(e)
-// {
-//   e.preventDefault();
-//   var result = document.getElementById('period');
-//
-//   var period = parseInt(form.period.value);
-//   var year = parseInt(form.year.value);
-//   var month = parseInt(form.month.value);
-//   var date = parseInt(form.date.value);
-//   var gender = parseInt(form.gender.value);
-//   var day;
-//
-//   function akanName(period, year, month, date)
-//   {
-//     day = ((  (period/4)-(2*period-1) +
-//               (5*year/4) +
-//               (26*(month+1)/10) +
-//               (date)) %7);
-//     return day;
-//   }
-//   if(gender === 1)
-//     {
-//       if (day === 0) {
-//         console.log("Your Akan name is Kwasi");
-//         alert("Your Akan name is Kwasi");
-//         // result.innerHTML = 'Your Akan name is Kwasi';
-//
-//       }else if (day === 1) {
-//         console.log("Your Akan name is Kwadwo");
-//         alert("Your Akan name is Kwadwo");
-//         // result.innerHTML = 'Your Akan name is Kwadwo';
-//
-//       }else if (day === 2) {
-//         console.log("Your Akan name is Kwabena");
-//         alert("Your Akan name is Kwabena");
-//         // result.innerHTML = 'Your Akan name is Kwabena';
-//
-//       }else if (day === 3) {
-//         console.log("Your Akan name is Kwaku");
-//         alert("Your Akan name is Kwaku");
-//         // result.innerHTML = 'Your Akan name is Kwaku';
-//
-//       }else if (day === 4) {
-//         console.log("Your Akan name is Yaw");
-//         alert("Your Akan name is Yaw");
-//         // result.innerHTML = 'Your Akan name is Yaw';
-//
-//       }else if (day === 5) {
-//         console.log("Your Akan name is Kofi");
-//         alert("Your Akan name is Kofi");
-//         // result.innerHTML = 'Your Akan name is Kofi';
-//
-//       }else {
-//         console.log("Your Akan name is Kwame");
-//         alert("Your Akan name is Kwame");
-//         // result.innerHTML = 'Your Akan name is Kwame';
-//
-//       }
-//     }
-//     else if (gender === 2) {
-//       if (day === 0) {
-//         console.log("Your Akan name is Akosua");
-//         alert("Your Akan name is Akosua");
-//       }else if (day === 1) {
-//         console.log("Your Akan name is Adwoa");
-//         alert("Your Akan name is Adwoa");
-//       }else if (day === 2) {
-//         console.log("Your Akan name is Abenaa");
-//         alert("Your Akan name is Abenaa");
-//       }else if (day === 3) {
-//         console.log("Your Akan name is Akua");
-//         alert("Your Akan name is Akua");
-//       }else if (day === 4) {
-//         console.log("Your Akan name is Yaa");
-//         alert("Your Akan name is Yaa");
-//       }else if (day === 5) {
-//         console.log("Your Akan name is Afua");
-//         alert("Your Akan name is Afua");
-//       }else {
-//         console.log("Your Akan name is Ama");
-//         alert("Your Akan name is Ama");
-//       }
-//     }
-//
-//   form.reset();
-// };
